843313d4a07f2e30f0a2469b482a6f06b8314cc9
Shefali Friesen <musictechgirl@gmail.com>
2015-02-11 12:21:07 -0500

    4 out of 5 tests passing

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -11,27 +11,54 @@
 @implementation StringCheese
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
-    /* WORK HERE */
-    return nil;
+    NSString* myFavorite = [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
+    NSLog(@"%@", myFavorite);
+    return myFavorite;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
-    /* WORK HERE */
-    return nil;
+    NSMutableString *modifiableCheeseString = [cheeseName mutableCopy];
+    NSRange cheeseRange = [modifiableCheeseString rangeOfString:@" cheese" options:NSCaseInsensitiveSearch];
+    if ([modifiableCheeseString rangeOfString:@"cheese"].location == NSNotFound)
+    {
+        NSLog(@"cheese string does not contain 'cheese'");
+    } else {
+    [modifiableCheeseString deleteCharactersInRange:cheeseRange];
+    }
+    NSLog(@"%@", modifiableCheeseString);
+//    NSLog(@"The location of 'cheese' : %ld", cheeseRange.location);
+//    NSLog(@"NSNotFound: %ld", NSNotFound);
+    return modifiableCheeseString;
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
+    NSString * cheeseAmount = [NSString stringWithFormat:@"%li cheeses", cheeseCount];
     if (cheeseCount == 1) {
-        /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+//        NSLog(@"%lu cheese", cheeseAmount);
+        NSLog(@"%@ cheese", cheeseAmount);
+//        NSMutableString *modifiedCheeseAmount = [cheeseAmount mutableCopy];
+//        NSString *updatedCheeseString = (@"%@", modifiedCheeseAmount);
+//        NSLog(@"%@", updatedCheeseString);
     } else {
-        /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+        NSLog(@"%@ cheeses", cheeseAmount);
+//        NSLog(@"The number of cheeses is %lu", cheeseCount);
+//        NSString * cheeseAmount = [NSString stringWithFormat:@"%li cheeses", cheeseCount];
+//    NSLog(@"%@", cheeseAmount);
+        
     }
+//    if (cheeseCount == 1){
+//        NSString * finalCheeseCount = @"%@ cheese", *cheeseAmount;
+//        NSLog(@"%@", finalCheeseCount);
+//    }
+//    else{
+//        NSString * finalCheeseCount = @"%@ cheeses", *cheeseAmount;
+//        NSLog(@"%@", finalCheeseCount);
+//    }
     
     /*
      (You will learn more about if/else statements in the next checkpoint.)
      */
-    
-    return nil;
+    return @"%@", cheeseAmount;
 }
 
 @end
