23e844e71628bf47d2ee6e94c7c992bf9fa7a14b
Shefali Friesen <musictechgirl@gmail.com>
2015-02-12 11:08:41 -0500

    fixed equality tests

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -11,26 +11,21 @@
 @implementation EqualityDeterminer
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
-    NSString *place1 = @"mountain";
-    NSString *place2 = @"river";
-    BOOL compareStrings = [place1 isEqualToString:place2];
-    NSLog(@"Are these strings the same? %@", compareStrings ? @"Yes": @"No");
-    return (YES);
+    BOOL areTheStringsEqual = [string1 isEqualToString:string2];
+    NSLog(@"Are these strings the same? %@", areTheStringsEqual ? @"Yes": @"No");
+    return areTheStringsEqual;
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
-    NSNumber *myNum1 = [NSNumber numberWithInt:(11)];
-    NSNumber *myNum2 = [NSNumber numberWithInt:(100)];
-    BOOL compareNumbers = [myNum1 isEqualToNumber:myNum2];
-    NSLog(@"Are these numbers the same? %@", compareNumbers ? @"Yes": @"No");
-    return (YES);
+    BOOL areTheNumbersEqual = [number1 isEqualToNumber:number2];
+    NSLog(@"Are these numbers the same? %@", areTheNumbersEqual ? @"Yes": @"No");
+    return areTheNumbersEqual;
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
-    NSInteger *myNum1 = 11;
-    NSInteger *myNum2 = 100;
-    NSLog(@"11 is greater than 100? %@", myNum1 > myNum2 ? @"Yes" : @"No");
-    return YES;
+    BOOL whichNumberisGreater = (integer1 > integer2);
+//    NSLog(@"Is %@ greater than %@?", integer1 > integer2 ? @"Yes" : @"No");
+    return whichNumberisGreater;
 }
 
 @end
