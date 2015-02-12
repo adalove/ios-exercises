492e2772100b98f59ca23f300aa04326f5de116a
Shefali Friesen <musictechgirl@gmail.com>
2015-02-11 22:12:03 -0500

    Completed equality tests

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -11,18 +11,26 @@
 @implementation EqualityDeterminer
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
-    /* WORK HERE */
-    return (string1 == string2);
+    NSString *place1 = @"mountain";
+    NSString *place2 = @"river";
+    BOOL compareStrings = [place1 isEqualToString:place2];
+    NSLog(@"Are these strings the same? %@", compareStrings ? @"Yes": @"No");
+    return (YES);
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
-    /* WORK HERE */
-    return (number1 == number2);
+    NSNumber *myNum1 = [NSNumber numberWithInt:(11)];
+    NSNumber *myNum2 = [NSNumber numberWithInt:(100)];
+    BOOL compareNumbers = [myNum1 isEqualToNumber:myNum2];
+    NSLog(@"Are these numbers the same? %@", compareNumbers ? @"Yes": @"No");
+    return (YES);
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
-    /* WORK HERE */
-    return NO;
+    NSInteger *myNum1 = 11;
+    NSInteger *myNum2 = 100;
+    NSLog(@"11 is greater than 100? %@", myNum1 > myNum2 ? @"Yes" : @"No");
+    return YES;
 }
 
 @end
