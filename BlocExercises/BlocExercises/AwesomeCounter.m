b51446ba495eea67965c162592c647eae5899b4f
Shefali Friesen <musictechgirl@gmail.com>
2015-02-16 21:44:42 -0500

    loop tests

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,35 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+    
+    NSMutableString * myNewNumber = [[NSMutableString alloc] init];
+    
+    if (number < otherNumber)
+    {
+        while (number <= otherNumber)
+        [myNewNumber appendFormat:@"%ld", number++];
+    
+        NSLog(@"the number is now %ld", number);
+    }
+    else if (number > otherNumber)
+    {
+        while (number >= otherNumber)
+        [myNewNumber appendFormat:@"%ld", otherNumber++];
+        NSLog(@"the number is now %ld", otherNumber);
+    }
+        
+    else
+    {
+        [myNewNumber appendFormat:@"%ld", number];
+    }
+//    
+//    NSString * newNumber = [NSString stringWithFormat:@"%ld", (long)number];
+    return myNewNumber;
+    
 }
 
 @end
+
+
+
+// NSString* myFavorite = [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
