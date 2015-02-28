67d5e1dcaac40abfed7ba9872b6b6498f3ce272c
Shefali Friesen <musictechgirl@gmail.com>
2015-02-27 20:59:20 -0500

    2 / 3 number handler tests

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -10,19 +10,65 @@
 
 @implementation NumberHandler
 
-- (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
-}
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
+    NSInteger firstNumber = number;
+    NSInteger lastNumber = otherNumber;
+    NSArray *myNumbers = @[@(firstNumber)];
+    NSMutableArray *myMutableArrayOfNumbers = [myNumbers mutableCopy];
+
+    
+    if (firstNumber < lastNumber)
+    {
+        
+        while (firstNumber < lastNumber)
+        {
+            firstNumber++;
+            NSNumber *nextNumber = [NSNumber numberWithInteger:(firstNumber)];
+            [myMutableArrayOfNumbers addObject:nextNumber];
+        }
+    }
+    else if (firstNumber > lastNumber)
+        {
+            while (firstNumber > lastNumber)
+            {
+            firstNumber--;
+            NSNumber *nextNumber = [NSNumber numberWithInteger:(firstNumber)];
+            [myMutableArrayOfNumbers addObject:nextNumber];
+            }
+            
+        }
+    else
+    {
+        NSNumber *nextNumber = [NSNumber numberWithInteger:(firstNumber)];
+        [myMutableArrayOfNumbers addObject:nextNumber];
+    }
+      return myMutableArrayOfNumbers;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+    NSNumber * firstNumber = arrayOfNumbers[0];
+    NSNumber * lowestNumber;
+    for (NSInteger idx = 0; idx < arrayOfNumbers.count; idx++)
+        {
+            if (arrayOfNumbers[idx] < firstNumber)
+            {
+                lowestNumber = arrayOfNumbers[idx];
+                NSLog(@"now the lowest number is %@", lowestNumber);
+            }
+            else if (arrayOfNumbers[idx] >= firstNumber)
+            {
+                lowestNumber = arrayOfNumbers[0];
+            }
+        }
+
+    int lowerNumber = [lowestNumber intValue];
+    return lowerNumber;
+}
+
+- (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
+    number = @([number intValue] * 2);
+    return number;
 }
 
 @end
--- BlocExercises/BlocExercises/NumberHandlerTests.m
+++ BlocExercises/BlocExercises/NumberHandlerTests.m
@@ -1,4 +1,4 @@
-//
+ //
 //  NumberHandlerTests.m
 //  BlocExercises
 //
